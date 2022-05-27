bdce34923ba53044743b21effc1c0e405e081fd8
heinu112 <81513657+heinu112@users.noreply.github.com>
2022-05-27 21:09:59 +0800

    Add files via upload

---
+++ customize.sh
@@ -0,0 +1,3 @@
+#!/system/bin/sh
+SKIPUNZIP=0
+set_perm_recursive $MODPATH 0 0 0777 777
---
+++ service.sh
@@ -0,0 +1,37 @@
+#!/system/bin/sh
+source ${0%/*}/module.ini
+stop=0
+start=0
+while true
+do
+    for package in ${packagelist}
+    do
+    if [[ $(pidof $package) ]];then
+    disable="true"
+    break
+    else
+    disable="false"
+    fi
+    done
+    if [ $disable == "true" ];then
+    if [ $start == "0" ];then
+    {
+    for disablepackage in ${disablepackagelist}
+    do
+    pm disable-user ${disablepackage}
+    done }&
+    stop=0
+    start=1
+    fi
+    else
+    if [ $stop == "0" ];then
+    {
+    for disablepackage in ${disablepackagelist}
+    do
+    pm enable ${disablepackage}
+    done }&
+    fi
+    stop=1
+    start=0
+    fi
+done
